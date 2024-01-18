eec03b8245435f4aae13a614844b0fe55f66834d
jtaylor2023 <jtaylor2023@48SE46-HM5HHZ3>
2024-01-18 16:25:28 +0100

    game improved; replay feature implemented

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+let mainContainer=document.querySelector(".main-container");
+let winnerInfo = document.querySelector(".winner-info");
+let replayInfo = document.querySelector(".replay-div");
 let info = document.querySelector(".info");
 let currentPlayer="X";
 let spaces = document.querySelectorAll(".space");
 let winner=null;
-let tieGame="false"
+let tieGame="false";
 const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -13,8 +16,15 @@ const winningCombinations = [
     [0, 4, 8],
     [2, 4, 6]
 ]
+winnerInfo.textContent = "Au tour de " + currentPlayer;
 
 spaces.forEach(space => {
+    space.addEventListener("mouseover", ()=>{
+        space.style.border ="0.5px solid black"
+    })
+    space.addEventListener('mouseleave',()=>{
+        space.style.border =""
+    })
     space.addEventListener("click", ()=>{
         if (currentPlayer=="X"){
         space.textContent="X";} else{
@@ -33,13 +43,15 @@ function switchPlayer(){
     if(winner==="X" || winner==="O" ||tieGame===true)
     {return} else{
     currentPlayer = currentPlayer==="X"?"O" : "X";
-    info.innerHTML = `<p>Au tour de ${currentPlayer}</p>`}
+    winnerInfo.textContent = "Au tour de " + currentPlayer;
+}
 }
 
 function checkForVictory() {
     for (let winningCombination of winningCombinations) {
         const [a, b, c] = winningCombination;
         if (spaces[a].textContent === "X" && spaces[b].textContent === "X" && spaces[c].textContent === "X") {
+
             return winner ="X"
         } else if (spaces[a].textContent === "O" && spaces[b].textContent === "O" && spaces[c].textContent === "O") {
             return winner="O"
@@ -57,13 +69,21 @@ function determineTie() {
 
 function displayWinner(){
     if(winner==="X"){
-        info.innerHTML =`<p>Joueur X, vous remportez la partie</p>`;
-        console.log("Winner: X")
+        winnerInfo.textContent ="Joueur X, vous remportez la partie";
+        endGame();
     } else if(winner==="O"){
-        info.innerHTML =`<p>Joueur O, vous remportez la partie</p>`}
+        winnerInfo.textContent ="Joueur O, vous remportez la partie";
+        endGame();}
     else if(tieGame===true){
-        info.innerHTML =`<p>Match nul</p>`
+        winnerInfo.textContent ="Match nul";
+        endGame();
     } else{return}
 }
 
+function endGame(){
+    mainContainer.innerHTML=``
+    info.style.backgroundColor="rgb(237, 210, 255)";
+    info.style.color="black";
+    info.style.display="flex"
+}
 
